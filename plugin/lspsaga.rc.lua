4e0959015ac9d9e5d133431eca8a7dfe046266cc
wangxinfu.wxf <wangxinfu.wxf@bytedance.com>
2023-10-09 21:21:11 +0800

    update

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -6,10 +6,9 @@ saga.setup {
 }
 
 local tops = { noremap = true, silent = ture }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga finder<cr>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga goto_definition<cr>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
 
