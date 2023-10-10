c6c8c6bf400f4ae1ddf1b644293ad6018697e042
wangxinfu.wxf <wangxinfu.wxf@bytedance.com>
2023-10-10 16:22:37 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,12 +28,4 @@ packer.startup(function(use)
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
-
-  use {
-    'OscarCreator/rsync.nvim',
-    run = 'make',
-    config = function()
-        require("rsync").setup()
-    end
-  }
 end)
