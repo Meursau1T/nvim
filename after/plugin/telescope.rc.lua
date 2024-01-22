fdfdad3768ce16528a61c2d7098af361a6405622
wangxinfu.wxf <wangxinfu.wxf@bytedance.com>
2024-01-22 18:40:26 +0800

    添加BUFFER删除能力

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -38,6 +38,18 @@ telescope.setup {
         }
       }
     }
+  },
+  pickers = {
+    buffers = {
+      show_all_buffers = true,
+      sort_lastused = true,
+      theme = "dropdown",
+      mappings = {
+        i = {
+          ["<c-d>"] = "delete_buffer",
+        }
+      }
+    }
   }
 }
 
