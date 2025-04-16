a2a288f567ff17fb724bfee2988107ac81d1fb27
MeursaulT <meursaultw@outlook.com>
2025-04-16 17:21:42 +0800

    update plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -107,44 +107,6 @@ require("lazy").setup({
       { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     },
   },
-  {
-    -- dir = "~/source/rsync-git.nvim",
-    "Meursau1T/rsync-git.nvim",
-    config = function()
-      require("rsync-git").setup {
-        rules = {
-          {
-            cond = "douyin_web/",
-            localPath = "/home/meursault/workspace/douyin_web",
-            remotePath = "/cloudide/workspace/douyin_web",
-            userIp = "cloudide-ws1f2f9f5f9c5d8672",
-          },
-          {
-            cond = "douyin_web_1/",
-            localPath = "/home/meursault/workspace/douyin_web_1",
-            remotePath = "/cloudide/workspace/douyin_web",
-            userIp = "cloudide-ws18a5a9be721c2d39",
-          },
-          {
-            cond = "douyin_web_2/",
-            userIp = "cloudide-ws502726de974cf8f5",
-            localPath = "/home/meursault/workspace/douyin_web_2",
-            remotePath = "/cloudide/workspace/douyin_web",
-          },
-          {
-            cond = "douyin_mobile/",
-            userIp = "cloudide-ws18a5a9be721c2d39",
-            localPath = "/home/meursault/workspace/douyin_mobile",
-            remotePath = "/cloudide/workspace/douyin_mobile",
-          }
-        },
-        config = {
-          showLog = true,
-          ignoreKeyword = "aweme_idl",
-        }
-      }
-    end
-  }
 })
 
 
