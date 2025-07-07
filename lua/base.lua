c92edf774c7c01b93705ba7f0d35b545bff0729d
MeursaulT <meursaultw@outlook.com>
2025-07-07 18:13:24 +0800

    4空格缩进

--- lua/base.lua
+++ lua/base.lua
@@ -41,9 +41,9 @@ vim.cmd([[
     set wildmenu
     set expandtab
 ]])
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.wildmode = 'longest:list'
 
 
